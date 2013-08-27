28e55666a00486a437fbff58fe352ca725296af0
akh2103 <akh2103@gmail.com>
2013-08-27 08:48:05 -0500

    dont add bad mines to explore

--- setNumbers.js
+++ setNumbers.js
@@ -25,3 +25,19 @@ function setNumbers(row, col){
    gameMines.push("c5-7");
    gameMines.push("c6-1");
    gameMines.push("c7-3");
+   
+   
+   
+    /*Removes any IDs from array that do not exist*/
+    function removeNonexistentIDs(id, IDs){
+         
+         for (var j=0;j<IDs.length;j++){
+              var myElem = document.getElementById(IDs[j]);
+              if (myElem == null){                      //if element does not exist
+                 var index = IDs.indexOf(IDs[j]) ;     //then find the index of the id 
+                 IDs.splice(index, 1);             //and pull it out of the array
+                }
+         }
+         return IDs;
+    }
+
